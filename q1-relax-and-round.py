1d0ecabab4e641e82003e7a13bc4bf0df2f6fe94
Caleb Johnson <calebdeejohnson@gmail.com>
2021-12-14 23:06:59 -0700

    Attempted to fix LPSolver steps

--- q1-relax-and-round.py
+++ q1-relax-and-round.py
@@ -1,4 +1,7 @@
 import random
+from scipy.optimize import linprog
+import pulp as pl
+
 
 m = n = 500
 d = 25
@@ -8,5 +11,25 @@ for i in range(n):
 	skill_i = set()
 	while len(skill_i) < d:
 		skill_i.add(random.randint(0, m))
-	people.append(skill_i)
-	print(skill_i)
+	people.append(list(skill_i))
+demand_vars = []
+
+prob = pl.LpProblem("LSCP", pl.LpMinimize)
+#Objective
+
+trav = 0
+var = {}
+for peo in people:
+	var[trav] = pl.LpVariable("x"+str(trav), lowBound = 0)
+
+prob += [1]*m
+for i in range(m):
+	prob += var[i] >= 0
+	prob += var[i] <= 1
+
+prob.solve()
+
+# c = [1]*n
+# bounds = [(0,1) for i in range(m)]
+# print(len(people))
+# linprog(c, bounds=bounds)
